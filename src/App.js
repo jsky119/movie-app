0d2cc6742b7ca657de7dbd3ebf2042c6aa196dc4
jsky119 <87289235+jsky119@users.noreply.github.com>
2021-08-23 20:22:47 +0900

    Docs: Update App.js
    delete test code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import React from "react";
 
 class App extends React.Component {
-  state = {
-    count: 1,
-  };
-  plus = () => {
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState((current) => ({ count: current.count - 1 }));
-  };
   render() {
-    return (
-      <div>
-        <h1>The number is {this.state.count}</h1>
-        <button onClick={this.plus}>+</button>
-        <button onClick={this.minus}>-</button>
-      </div>
-    );
+    return <div />;
   }
 }
 
