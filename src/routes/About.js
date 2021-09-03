b4c0baa8debb3f434cc78fd796dfb2a549bab47a
jsky119 <87289235+jsky119@users.noreply.github.com>
2021-09-03 12:10:58 +0900

    Docs: Create about.css

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "./About.css";
 
 function About() {
-  return <span>About this page : React의 학습을 위하여 만들었습니다!</span>;
+  return (
+    <div className="about__container">
+      <span className="about__text">
+        이 사이트는 React의 학습을 위하여 제작하였습니다.
+      </span>
+    </div>
+  );
 }
 
 export default About;
